72d4ae5fc060805b9da633cf4dcf0dcda9278487
Daniel <todak2000@gmail.com>
2021-06-12 16:15:27 +0100

    skeletal job api created

--- api/views.py
+++ api/views.py
@@ -546,6 +546,49 @@ def profile(request,decrypedToken):
         }
     return Response(return_data)
 
+# DESIGN AND IMPLEMENT THE JOB PART ASAP***********************
+# @api_view(["GET"])
+# @autentication.token_required
+# def job(request,decrypedToken):
+#     try:
+#         user_id = decrypedToken['user_id']
+#         if user_id != None and user_id != '':
+#             #get user info
+#             user_data = User.objects.get(user_id=decrypedToken["user_id"])
+#             return_data = {
+#                 "success": True,
+#                 "status" : 200,
+#                 "message": "Successfull",
+#                 "user_details": 
+#                     {
+#                         "firstname": f"{user_data.firstname}",
+#                         "lastname": f"{user_data.lastname}",
+#                         "email": f"{user_data.email}",
+#                         "phonenumber": f"{user_data.phone}",
+#                         "address": f"{user_data.address}",
+#                         "state": f"{user_data.state}",
+#                         "role": f"{user_data.role}",
+#                         "accountname": f"{user_data.account_name}",
+#                         "accountno": f"{user_data.account_number}",
+#                         "bank": f"{user_data.bank_name}",
+#                         # "isVerified": f"{user_data.address}",
+#                     }
+#             }
+#         else:
+#             return_data = {
+#                 "success": False,
+#                 "status" : 201,
+#                 "message": "Invalid Parameter"
+#             }
+#     except Exception as e:
+#         return_data = {
+#             "success": False,
+#             "status" : 201,
+#             "message": str(e)
+#         }
+#     return Response(return_data)
+
+
 # EDIT BIO API
 @api_view(["PUT"])
 def edit_bio(request):
