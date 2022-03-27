25d2c77cd889dd68be9e1759e14a0869c1c2db3a
Daniel Olagunju <todak2000@gmail.com>
2022-03-27 10:52:14 +0100

    adding special request functionality

--- api/views.py
+++ api/views.py
@@ -950,6 +950,53 @@ def service_request(request):
     return Response(return_data)
 
 @api_view(["POST"])
+def special_request_admin(request):
+    specialRequest = Services.objects.filter(isDirectedToAdmin=True)
+    num = len(specialRequest)
+    specialRequestList = []
+    for i in range(0,num):
+        client_address = User.objects.get(user_id=specialRequest[i].client_id).address + " "+ User.objects.get(user_id=specialRequest[i].client_id).state
+        client_phone = User.objects.get(user_id=specialRequest[i].client_id).phone
+        client= User.objects.get(user_id=specialRequest[i].client_id).firstname + " "+ User.objects.get(user_id=specialRequest[i].client_id).lastname
+        client_email = User.objects.get(user_id=specialRequest[i].client_id).email
+        job_id = specialRequest[i].id
+        date_added = specialRequest[i].date_added
+        service_type  = specialRequest[i].service_type
+        description  = specialRequest[i].description
+        payment_mode = specialRequest[i].payment_mode
+        if payment_mode == "wallet":
+            isPaid = True
+        else:
+            isPaid = False
+        isCompleted = specialRequest[i].isCompleted
+        to_json = {
+            "client_address": client_address,
+            "client_phone": client_phone,
+            "client_name":client,
+            "client_email":client_email,
+            "job_id": job_id,
+            "job_description": description,
+            "service_type": service_type,
+            "hasPaid": isPaid,
+            "isCompleted": isCompleted,
+            "date_added": date_added,
+        }
+        specialRequestList.append(to_json)
+    if num > 0:
+        return_data = {
+            "success": True,
+            "status" : 200,
+            "message": "Successfull",
+            "specialRequestList": specialRequestList
+        }
+    if num <= 0:
+        return_data = {
+            "success": True,
+            "status" : 200,
+            "message": "Sorry! You have no Special Job requests"
+        }
+
+@api_view(["POST"])
 def special_service_request(request):
     user_phone = request.data.get("phone",None)
     service_type = request.data.get("service_type",None)
@@ -1064,6 +1111,7 @@ def services(request,decrypedToken):
                 isTaken  = userServices[i].isTaken
                 isRejectedSP = userServices[i].isRejectedSP
                 isCompleted = userServices[i].isCompleted
+                isSpecialRequest = userServices[i].isDirectedToAdmin
                 to_json = {
                     "sp_id": sp_id,
                     "client_id": client_id,
@@ -1072,6 +1120,7 @@ def services(request,decrypedToken):
                     "service_type": service_type,
                     "isRejectedSP": isRejectedSP,
                     "isCompleted": isCompleted,
+                    "isSpecialRequest":isSpecialRequest,
                     "date_added": date_added,
                 }
                 userServicesList.append(to_json)
